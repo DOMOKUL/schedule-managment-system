590ceda79049af34c8390572b856dab4e1abf54a
pokal <strokachnik@gmail.com>
2022-03-02 23:13:36 +0300

    add mappedSuperClass

--- src/main/java/com/company/schedule/managment/system/models/Person.java
+++ src/main/java/com/company/schedule/managment/system/models/Person.java
@@ -3,6 +3,9 @@ package com.company.schedule.managment.system.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 @Data
 @NoArgsConstructor
 public abstract class Person {
