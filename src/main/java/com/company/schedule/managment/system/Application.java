f9119db61ff15fd39eaf831d668fa506eb3d2c50
Koryto Daniil <DKoryto@alfabank.ru>
2022-03-04 03:33:06 +1000

    added DataSourceConfiguration

--- src/main/java/com/company/schedule/managment/system/Application.java
+++ src/main/java/com/company/schedule/managment/system/Application.java
@@ -1,8 +1,13 @@
 package com.company.schedule.managment.system;
 
+import com.company.schedule.managment.system.config.DataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({
+        DataSourceProperties.class
+})
 @SpringBootApplication
 public class Application {
 
