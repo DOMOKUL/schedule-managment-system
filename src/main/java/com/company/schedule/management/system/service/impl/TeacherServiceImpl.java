049e5b5bd5069e468c0d93be504ef7cbdba4a2e3
pokal <strokachnik@gmail.com>
2022-04-17 21:26:01 +0300

    added annotation Transactional

--- src/main/java/com/company/schedule/management/system/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/company/schedule/management/system/service/impl/TeacherServiceImpl.java
@@ -9,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class TeacherServiceImpl implements TeacherService {
 
